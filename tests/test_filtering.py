c2dd9da46ff607b2ae908450ce53cb8fdae3aaf5
John Ragland <jhrag@uw.edu>
2023-08-07 23:47:50 +0000

    updated unit tests

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -2,8 +2,52 @@
 test filtering functions
 '''
 
+import xarray as xr
+import xrsignal as xrs
+import numpy as np
+from scipy import signal
+
 def test_filtfilt():
-    assert True
+    # create some data
+
+    x = np.random.rand(1000,10)
+    xx = xr.DataArray(x, dims=['time', 'distance'], coords={'time':np.arange(1000), 'distance':np.arange(10)})
+    b,a = signal.butter(3, 0.5, 'low')
+
+    ## test unchunked operation
+    xx_xrs_filt = xrs.filtfilt(xx, dim='time', b=b, a=a)
+    xx_sig_filt = signal.filtfilt(b, a, x, axis=0)
+    
+    # make sure all values are the same
+    assert np.sum(xx_xrs_filt.values == xx_sig_filt) == 10000
+    assert type(xx_xrs_filt) == xr.core.dataarray.DataArray
+    assert xx_xrs_filt.shape == (1000,10)
+
+    ## test chunked operation
+    xx = xx.chunk({'time':100, 'distance':1})
+    xx_xrs_filt = xrs.filtfilt(xx, dim='time', b=b, a=a)
+    assert type(xx_xrs_filt) == xr.core.dataarray.DataArray
+    assert xx_xrs_filt.shape == (1000, 10)
+
 
 def test_hilbert():
-    assert True
+    x = np.random.rand(1000,10)
+    xx = xr.DataArray(x, dims=['time', 'distance'], coords={'time':np.arange(1000), 'distance':np.arange(10)})
+    xx_chunk = xx.chunk({'time':100, 'distance':1})
+    ds = xr.Dataset({'xx':xx})
+    ds_chunk = xr.Dataset({'xx':xx_chunk})
+
+    # compare values to scipy.signal.hilbert
+    xx_xrs_hilb = xrs.hilbert(xx, dim='time')
+    xx_sig_hilb = signal.hilbert(x, axis=0)
+    assert(np.sum(xx_xrs_hilb.values == xx_sig_hilb) == 10000)
+    assert(type(xx_xrs_hilb) == xr.core.dataarray.DataArray)
+
+    ds_xrs_hilb = xrs.hilbert(ds, dim='time').compute()
+    assert(type(ds_xrs_hilb) == xr.core.dataset.Dataset)
+
+    ds_xrs_hilb_chunk = xrs.hilbert(ds_chunk, dim='time').compute()
+    assert(type(ds_xrs_hilb_chunk) == xr.core.dataset.Dataset)
+
+    xx_chunk_xrs_hilb = xrs.hilbert(xx_chunk, dim='time')
+    assert(type(xx_chunk_xrs_hilb) == xr.core.dataarray.DataArray)
--- tests/test_spectral.py
+++ tests/test_spectral.py
@@ -3,36 +3,33 @@ import xarray as xr
 import numpy as np
 
 def test_csd():
-    assert True
-    '''
-    test csd
- 
 
     # create some data
-    x = np.random.rand(100,100,100)
-    y = np.random.rand(100,100,100)
+    x = np.random.rand(1000,50,50)
+    y = np.random.rand(1000,50,50)
 
-
-    xx = xr.DataArray(x, dims=['time','lat','lon'], coords={'time':np.arange(100),'lat':np.arange(100),'lon':np.arange(100)})
-    yx = xr.DataArray(y, dims=['time','lat','lon'], coords={'time':np.arange(100),'lat':np.arange(100),'lon':np.arange(100)})
+    xx = xr.DataArray(x, dims=['time','lat','lon'], coords={'time':np.arange(1000),'lat':np.arange(50),'lon':np.arange(50)})
+    yx = xr.DataArray(y, dims=['time','lat','lon'], coords={'time':np.arange(1000),'lat':np.arange(50),'lon':np.arange(50)})
     ds = xr.Dataset({'xx':xx, 'yx':yx})
-    ds = ds.chunk({'time':10, 'lat':10, 'lon':10})
-    # test csd
-    csd = xrs.csd(ds,dim='time', window='hann').compute()
+    ds = ds.chunk({'time':50, 'lat':50, 'lon':50})
+    csd = xrs.csd(ds, dim='time', window='hann', nperseg=10, noverlap=5).mean('time')
 
-    print(csd)
-    assert csd.shape == (100,100,100)
-    '''
+    assert type(csd) == xr.core.dataarray.DataArray
+    assert csd.shape == (6, 50, 50)
+
 def test_welch():
     assert True
-    '''
-    test welch method
-    
-
     # create some data
-    x = np.random.rand(100,100,100)
-    xx = xr.DataArray(x, dims=['time','lat','lon'], coords={'time':np.arange(100),'lat':np.arange(100),'lon':np.arange(100)})
-    xx = xx.chunk({'time':10, 'lat':10, 'lon':10})
-    # test welch
-    XX = xrs.csd(xx,dim='time', window='hann').compute()
-    '''
+    x = np.random.rand(1000,50,50)
+
+
+    xx = xr.DataArray(x, dims=['time','lat','lon'], coords={'time':np.arange(1000),'lat':np.arange(50),'lon':np.arange(50)})
+    xx = xx.chunk({'time':50, 'lat':50, 'lon':50})
+    ds = xr.Dataset({'xx':xx})
+    psd_ds = xrs.welch(ds, dim='time', window='hann', nperseg=10, noverlap=5).mean('time')
+    psd_da = xrs.welch(xx, dim='time', window='hann', nperseg=10, noverlap=5).mean('time')
+
+    assert type(psd_da) == xr.core.dataarray.DataArray
+    assert type(psd_ds) == xr.core.dataset.Dataset
+    assert psd_da.shape == (6, 50, 50)
+    assert psd_ds['xx'].shape == (6, 50, 50)
